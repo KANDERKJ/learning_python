6feea2475e94c25c2d3cc729f223af776bb1bd0d
KANDERKJ <68558542+KANDERKJ@users.noreply.github.com>
2020-07-21 12:00:55 -0700

    Add files via upload

---
+++ list_practice.py
@@ -0,0 +1,14 @@
+
+integers = [1, 2, 4, 5, 10] # a list of integers
+print(integers)
+integers.append(100)    # adds 100 to end of list, when you append it adds to the end
+print(integers)
+integers.append(6)      # adds 6 to the end of list
+print(integers)
+
+integers.pop(6)         #removes 6 from end of list
+print(integers)         # pop(self, index) you can target which element for removing
+
+integers.pop()          #removes last integer of lsit by default
+print(integers)
+
