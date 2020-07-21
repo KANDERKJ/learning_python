5ab0b7cee1593f1ebce7ba713bbd24cd21e69348
KANDERKJ <68558542+KANDERKJ@users.noreply.github.com>
2020-07-21 10:43:12 -0700

    Add files via upload

---
+++ enter_integer.py
@@ -0,0 +1,7 @@
+# to read in an integer and double it
+# python script.py
+
+user_input = int(input('Type in an integer ')) # converts user input to int
+double = user_input * 2 # multiply user input by 2
+# print('User input = {}'.format(double))
+print('You entered {}: Result doubled = {}'.format(user_input, double))
