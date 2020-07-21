65061045dc932993223ded852c9dfb7d1d3f3775
KANDERKJ <68558542+KANDERKJ@users.noreply.github.com>
2020-07-21 11:13:40 -0700

    Add files via upload

---
+++ string_practice.py
@@ -0,0 +1,13 @@
+# Shift
+
+lang = 'python'
+lang = lang.upper() # makes string uppercase
+print(lang)
+lang = lang.capitalize() # makes first letter capital
+print(lang)
+lang[2].upper()
+
+import math
+
+a = math.gcd(2733, 166136163)
+print(a)
