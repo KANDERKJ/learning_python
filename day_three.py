bf7a83e2781a5358fe665f4114668e935f5f5d7b
KANDERKJ <68558542+KANDERKJ@users.noreply.github.com>
2020-07-22 13:17:16 -0700

    Add files via upload

---
+++ day_three.py
@@ -0,0 +1,46 @@
+import math
+
+# create two variables a, b that's set equal to some numbers and print the following:
+
+a, b = 10, 25
+print(a + b)    # 35
+print(a * b)    # 250
+print(a - b)    # -15
+print(a / b)    # 0.4
+print(a % b)    # 10
+
+# create a string x and set it equal to 'python' Do the following:
+
+x = 'python'
+print(x[0])
+print([-1])
+print(len(x))
+print(x.upper())
+x = x.upper()
+print(x)
+
+
+# create a list called nums that by default is empty
+
+nums = []
+nums.append(10)
+nums.append(100)
+nums.append(10)
+nums.append(500)
+nums.append(56)
+print(nums)
+nums.pop()
+print(nums)
+nums.sort()
+print(nums)
+
+
+
+
+
+
+
+
+
+
+
